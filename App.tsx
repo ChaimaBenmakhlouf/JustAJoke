87f2c023cb0859aa0d18f9fdac887b406d2900c3
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2022-11-08 10:19:28 +0100

    Installed Native Base

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Box, NativeBaseProvider, Text } from "native-base"
+import React from "react"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NativeBaseProvider>
+			<Box safeArea>
+				<Text color="blue.500">Hello world</Text>
+			</Box>
+		</NativeBaseProvider>
+	)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
