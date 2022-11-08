7d0dacabd11848a170f689057a49eca7de0db9e9
Nasake <wilfrid.salles-otta@supinternet.fr>
2022-11-08 11:30:33 +0100

    ProfilPage1

---
+++ src/Router/Screens/ProfilScreen.tsx
@@ -0,0 +1,8 @@
+import { View } from "native-base"
+import React from "react"
+
+const ProfilScreen = () => {
+	return <View></View>
+}
+
+export default ProfilScreen
--- src/Router/TabBarNavigator.tsx
+++ src/Router/TabBarNavigator.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import HomeStackScreens from "./Stacks/HomeStackScreens"
+import ProfilStackScreens from "./Stacks/ProfilStackScreens"
 
 const Tab = createBottomTabNavigator()
 
@@ -8,6 +9,7 @@ const TabBarNavigator = () => {
 	return (
 		<Tab.Navigator screenOptions={{ headerShown: false }}>
 			<Tab.Screen name="HomeStack" component={HomeStackScreens} />
+			<Tab.Screen name="ProfilStack" component={ProfilStackScreens} />
 		</Tab.Navigator>
 	)
 }
