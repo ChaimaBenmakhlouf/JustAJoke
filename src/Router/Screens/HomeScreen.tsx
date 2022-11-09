867d6e060d98f318e2cadf0f5296279d06da4a51
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2022-11-09 02:33:51 +0100

    Almost finished page + added animation

--- src/Router/Screens/HomeScreen.tsx
+++ src/Router/Screens/HomeScreen.tsx
@@ -1,8 +1,28 @@
-import { Box, View } from "native-base"
+import { Avatar, Box, Center, Flex, HStack, Icon, Image, Input, Pressable, ScrollView, Text, VStack } from "native-base"
 import React from "react"
+import { Path } from "react-native-svg"
+import Header from "../../Components/HomeScreen/Header"
+import JokeCard from "../../Components/HomeScreen/JokeCard"
 
 const HomeScreen = () => {
-	return <Box></Box>
+	return (
+		<>
+			<Header />
+			<ScrollView pt="4" px="4">
+				<HStack alignItems="center" flexDir="row" display="flex">
+					<Avatar bg="green.600" size="sm">
+						EL
+					</Avatar>
+
+					<Input ml="4" borderRadius={"full"} placeholder="Commencez à blaguer..." borderColor="#E3E3E3" borderWidth={"1"} bg="#FDFDFD" width="xs" />
+				</HStack>
+
+				<Box py="4" width="full">
+					<JokeCard />
+				</Box>
+			</ScrollView>
+		</>
+	)
 }
 
 export default HomeScreen
