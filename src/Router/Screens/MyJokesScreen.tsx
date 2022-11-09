e4326d5357f71f167828b482deac9c0c72f25ec8
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2022-11-10 00:07:09 +0100

    Updated jokes screen

--- src/Router/Screens/MyJokesScreen.tsx
+++ src/Router/Screens/MyJokesScreen.tsx
@@ -1,169 +1,175 @@
-import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
-import { Button } from 'native-base';
-import React, { useState } from "react";
-import Constants from 'expo-constants';
-
+import { Box, Button, Flex, HStack, Image, Pressable, ScrollView, Text, TextArea, VStack } from "native-base"
+import React, { useState } from "react"
+import Constants from "expo-constants"
 
 const MyJokesScreen = () => {
-    const [data, setData] = useState([{ name: '', response: '' }]);
-    const [name, setName] = useState('');
-    const [response, setResponse] = useState('');
-    return (
-        <View>
-            <View>
-                <View style={{flexDirection:"row"}}>
-                    <View style={{flex:1}}>
-                        <Text style={styles.title}>Mes <Text style={styles.text}>blagues</Text> </Text>
-                        <Text style={styles.title}>postées</Text>
-                    </View>
-        
-        <View>
-            <Text style={styles.eth}>761 ETH</Text>
-        </View>
-                </View>
-            </View>
-        
-        <View style={styles.content}>
-            <View style={styles.containerText}>
-                <TextInput
-                    style={styles.inputQuestion}
-                    placeholder={'Commencez à blaguer...'}
-                    onChangeText={(name) => setName(name)}
-                    value={name}
-        
-                />
-                 <TextInput
-                    style={styles.inputResponse}
-                    placeholder={'Reponse...'}
-                    onChangeText={(response) => setResponse(response)}
-                    value={response}
-        
-                />
-        
-            </View>
-        
-        <Text style={{color:'#FF9839', marginLeft: 30, marginTop:50, fontSize:10}}>Encore 1 blague disponible pour aujourd’hui*</Text>
-        
-        
-        <Button style={styles.btn} onPress={() => {
-            if (name) setData([...data, { name: name, response: response }]);
-            
-            
-        } } >Publier</Button>
-        
-        </View>
-        
-        <View>
-            <View>
-                <FlatList 
-                keyExtractor={(item) => item.name}
-                data={data}
-        
-                renderItem={({ item }) => (
-        <View style={styles.content}>
-            
-            <Text>{item.name}</Text>
-            <Text style={styles.textResponse}>{item.response}</Text>
-            <Button style={styles.btnLike}>25</Button>
-           
+	const [data, setData] = useState([{ name: "", response: "" }])
+	const [name, setName] = useState("")
+	const [response, setResponse] = useState("")
+	return (
+		<>
+			<Box bg="white" borderBottomRadius="3xl" shadow="4" height="4" width="full" />
+			<ScrollView>
+				<Box p="4">
+					<VStack space="6" bg="white" p="4" borderRadius={"xl"}>
+						{/* @ts-ignore */}
+						<TextArea h={"24"} borderWidth={0} borderRadius="xl" bg="#F5F5F5" placeholder="Commencez à blaguer..." />
 
- 
-        </View>
-       
-        
-        )}
-       
-      />
-        
-            </View>
-        </View>
-        
-        </View>
-        
-        
-        );
-    }
-    const styles = StyleSheet.create({
-        text: {
-            color: 'red',
-            
-        },
-        title: {
-            marginLeft: 30,
-            fontSize: 20,
-            fontWeight: "bold"
-        },
-        eth: {
-            
-            color: '#FFFFFF',
-            textAlign: 'center',
-            width: 80,
-            height: 30,
-            fontWeight: "bold",
-            backgroundColor: 'black',
-            borderRadius: 30,
-            
-        },
-        content: {
-            marginTop: 60,
-            backgroundColor: 'white',
-            height: 300,
-            shadowColor: '#171717',
-            shadowOffset: {width: -2, height: 4},
-            shadowOpacity: 0.2,
-            shadowRadius: 3,
-            borderRadius: 30,
-            
-            
-        },
-        contentText: {
-            fontWeight: "bold",
-            fontSize: 20,
-            marginTop: 20,
-        },
-        contentJoke: {
-            marginTop: 20,
-        },
-        containerText: {
-            marginLeft: 25,
-            flex: 1,
-            justifyContent: 'center',
-            paddingTop: Constants.statusBarHeight,
-            padding: 8,
-        },
-        inputQuestion: {
-            height: 100,
-            margin: 10,
-            marginTop: 30,
-            backgroundColor: '#D9D9D9',
-            borderRadius: 10,
-        },
+						<Flex alignItems="center" justifyContent={"space-between"} flexDir="row">
+							<Text color="#FF9839" fontSize={"2xs"}>
+								Encore 1 blagues disponible pour aujourd'hui*
+							</Text>
+							<Box
+								bg={{
+									linearGradient: {
+										colors: ["#FF9839", "#F95F5E"],
+										start: [0, 1],
+										end: [0.75, 0]
+									}
+								}}
+								borderRadius="full">
+								<Button size="sm" px="4" bg="whiteAlpha.100" borderRadius="full">
+									Publier
+								</Button>
+							</Box>
+						</Flex>
+					</VStack>
 
-        inputResponse: {
-            height: 100,
-            margin: 10,
-            marginBottom: 5,
-            backgroundColor: '#D9D9D9',
-            borderRadius: 10,
-        },
-        btn: {
-            backgroundColor:'#F95F5E',
-            width: 80,
-            borderRadius: 30,
-            marginLeft: 300,
-            marginBottom: 30,
-            
-        },
-        btnLike: {
-            backgroundColor:'#F95F5E',
-            width: 80,
-            borderRadius: 30,
-        },
-        textResponse: {
-            color: '#FF9839'
-        }
-        
-        
-    });
-    export default MyJokesScreen
-    
+					<Box p="4" mt="2" bg="white" borderRadius={"xl"} shadow="1">
+						<Text fontWeight={"bold"} fontSize="lg">
+							Blague du 12 Novembre
+						</Text>
+
+						<Text fontSize={"md"} color="black" pt="4">
+							Qu'est-ce qu'un cochon qui passe la douane ?
+						</Text>
+						<Text fontSize={"xs"} color="#FF9839" pt="4" fontWeight={"bold"}>
+							Un passeport
+						</Text>
+
+						<Flex justifyContent={"flex-end"} alignItems="flex-end">
+							<Pressable px="4" py="2" bg="white" borderColor="black" borderWidth={"1"} borderRadius={"full"}>
+								<HStack space="1.5" alignItems="center">
+									<Text color="black">25</Text>
+									<Image source={require("../../../assets/like.png")} alt="like button" />
+								</HStack>
+							</Pressable>
+						</Flex>
+					</Box>
+
+					<Box p="4" mt="2" bg="white" borderRadius={"xl"} shadow="1">
+						<Text fontWeight={"bold"} fontSize="lg">
+							Blague du 11 Novembre
+						</Text>
+
+						<Text fontSize={"md"} color="black" pt="4">
+							Qui est l'acteur à la main verte ?
+						</Text>
+						<Text fontSize={"xs"} color="#FF9839" pt="4" fontWeight={"bold"}>
+							Jean Dujardin
+						</Text>
+
+						<Flex justifyContent={"flex-end"} alignItems="flex-end">
+							<Pressable px="4" py="2" bg="white" borderColor="black" borderWidth={"1"} borderRadius={"full"}>
+								<HStack space="1.5" alignItems="center">
+									<Text color="black">145</Text>
+									<Image source={require("../../../assets/like.png")} alt="like button" />
+								</HStack>
+							</Pressable>
+						</Flex>
+					</Box>
+
+					<Box p="4" mt="2" bg="white" borderRadius={"xl"} shadow="1">
+						<Text fontWeight={"bold"} fontSize="lg">
+							Blague du 11 Novembre
+						</Text>
+
+						<Text fontSize={"md"} color="black" pt="4">
+							Qui est l'acteur à la main verte ?
+						</Text>
+						<Text fontSize={"xs"} color="#FF9839" pt="4" fontWeight={"bold"}>
+							Jean Dujardin
+						</Text>
+
+						<Flex justifyContent={"flex-end"} alignItems="flex-end">
+							<Pressable px="4" py="2" bg="white" borderColor="black" borderWidth={"1"} borderRadius={"full"}>
+								<HStack space="1.5" alignItems="center">
+									<Text color="black">145</Text>
+									<Image source={require("../../../assets/like.png")} alt="like button" />
+								</HStack>
+							</Pressable>
+						</Flex>
+					</Box>
+
+					<Box p="4" mt="2" bg="white" borderRadius={"xl"} shadow="1">
+						<Text fontWeight={"bold"} fontSize="lg">
+							Blague du 11 Novembre
+						</Text>
+
+						<Text fontSize={"md"} color="black" pt="4">
+							Qui est l'acteur à la main verte ?
+						</Text>
+						<Text fontSize={"xs"} color="#FF9839" pt="4" fontWeight={"bold"}>
+							Jean Dujardin
+						</Text>
+
+						<Flex justifyContent={"flex-end"} alignItems="flex-end">
+							<Pressable px="4" py="2" bg="white" borderColor="black" borderWidth={"1"} borderRadius={"full"}>
+								<HStack space="1.5" alignItems="center">
+									<Text color="black">145</Text>
+									<Image source={require("../../../assets/like.png")} alt="like button" />
+								</HStack>
+							</Pressable>
+						</Flex>
+					</Box>
+
+					<Box p="4" mt="2" bg="white" borderRadius={"xl"} shadow="1">
+						<Text fontWeight={"bold"} fontSize="lg">
+							Blague du 11 Novembre
+						</Text>
+
+						<Text fontSize={"md"} color="black" pt="4">
+							Qui est l'acteur à la main verte ?
+						</Text>
+						<Text fontSize={"xs"} color="#FF9839" pt="4" fontWeight={"bold"}>
+							Jean Dujardin
+						</Text>
+
+						<Flex justifyContent={"flex-end"} alignItems="flex-end">
+							<Pressable px="4" py="2" bg="white" borderColor="black" borderWidth={"1"} borderRadius={"full"}>
+								<HStack space="1.5" alignItems="center">
+									<Text color="black">145</Text>
+									<Image source={require("../../../assets/like.png")} alt="like button" />
+								</HStack>
+							</Pressable>
+						</Flex>
+					</Box>
+
+					<Box p="4" mt="2" bg="white" borderRadius={"xl"} shadow="1">
+						<Text fontWeight={"bold"} fontSize="lg">
+							Blague du 11 Novembre
+						</Text>
+
+						<Text fontSize={"md"} color="black" pt="4">
+							Qui est l'acteur à la main verte ?
+						</Text>
+						<Text fontSize={"xs"} color="#FF9839" pt="4" fontWeight={"bold"}>
+							Jean Dujardin
+						</Text>
+
+						<Flex justifyContent={"flex-end"} alignItems="flex-end">
+							<Pressable px="4" py="2" bg="white" borderColor="black" borderWidth={"1"} borderRadius={"full"}>
+								<HStack space="1.5" alignItems="center">
+									<Text color="black">145</Text>
+									<Image source={require("../../../assets/like.png")} alt="like button" />
+								</HStack>
+							</Pressable>
+						</Flex>
+					</Box>
+				</Box>
+			</ScrollView>
+		</>
+	)
+}
+
+export default MyJokesScreen
